e9ef5431e3acb1134ed39f931b9392367c622720
PREETI <preetikadyan634@gmail.com>
2021-03-22 21:36:40 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -1,5 +1,5 @@
 
-def  check_if_soc_or_charging_parameter_gives_noise(value, nextValue, maxDelta):
+def  check_if_given_parameter_produces_noise(value, nextValue, maxDelta):
   if nextValue - value > maxDelta:
     return False
   return True
@@ -9,7 +9,7 @@ def validate_reading(values):
   #if length != 0:  
       last_but_one_reading = len(values) - 1
       for i in range(last_but_one_reading):
-          if(not check_if_soc_or_charging_parameter_gives_noise(values[i], values[i + 1], 0.05)):
+          if(not check_if_given_parameter_produces_noise(values[i], values[i + 1], 0.05)):
               return False
       return True
 
