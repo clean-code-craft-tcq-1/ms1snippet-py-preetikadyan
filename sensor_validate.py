929ab93d27a251bd816cf403eabb93b6493f1484
PREETI <preetikadyan634@gmail.com>
2021-03-22 22:01:47 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -4,10 +4,12 @@ def  check_if_given_parameter_produces_noise(value, nextValue, maxDelta):
     return False
   return True
 
-def validate_reading(values):
-  #length = len(values)
-  #if length != 0:  
-      last_but_one_reading = len(values) - 1
+def check_if_length_is_not_none_and_validate_reading(values):
+    length = len(values)
+    if length != 0:
+       validate_reading(values,length - 1)
+  
+def validate_reading(values, last_but_one_reading):
       for i in range(last_but_one_reading):
           if(not check_if_given_parameter_produces_noise(values[i], values[i + 1], 0.05)):
               return False
