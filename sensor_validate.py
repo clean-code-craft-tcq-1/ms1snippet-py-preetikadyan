82efedb235d5940e274b9b89ef9b23858baec68e
PREETI <preetikadyan634@gmail.com>
2021-03-22 21:12:28 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -4,16 +4,12 @@ def  _give_me_a_good_name(value, nextValue, maxDelta):
     return False
   return True
 
-def validate_soc_reading(values):
-  last_but_one_reading = len(values) - 1
-  for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.05)):
-      return False
-  return True
+def validate_reading(values):
+  length = len(values)
+  if length != 0:  
+      last_but_one_reading = len(values) - 1
+      for i in range(last_but_one_reading):
+          if(not _give_me_a_good_name(values[i], values[i + 1], 0.05)):
+              return False
+      return True
 
-def validate_current_reading(values):
-  last_but_one_reading = len(values) - 1
-  for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.1)):
-      return False
-  return True
