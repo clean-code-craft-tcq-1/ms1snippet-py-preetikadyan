b7985c2d37372bf9f0addfbae1106a6dbf3ef02a
PREETI <preetikadyan634@gmail.com>
2021-03-24 11:40:53 +0530

    Update sensor_validate.test.py

--- sensor_validate.test.py
+++ sensor_validate.test.py
@@ -1,15 +1,19 @@
 import unittest
 import sensor_validate
 
+# Limit specification of battery Parameters
+soc_limit = 0.05
+current_limit = 0.1
+
 class SensorValidatorTest(unittest.TestCase):
   def test_reports_error_when_soc_jumps(self):
-    soc_limit = 0.05
+    
     self.assertFalse(
       sensor_validate.check_if_length_is_not_none_and_validate_reading([0.0, 0.01, 0.5, 0.51],soc_limit)
     )
   
   def test_reports_error_when_current_jumps(self):
-    current_limit = 0.1
+    
     self.assertFalse(
       sensor_validate.check_if_length_is_not_none_and_validate_reading([0.03, 0.03, 0.03, 0.33],current_limit)
     )
