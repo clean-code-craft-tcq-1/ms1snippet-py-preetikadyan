4cca3905bcb5f5505f7620aaf84c9cae43d3d9d9
PREETI <preetikadyan634@gmail.com>
2021-03-25 13:18:53 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -10,7 +10,7 @@ def check_if_length_is_not_none_and_validate_reading(values,limit,parameter_name
        print("--------------Validate Reading for the Parameter ",parameter_name,"--------------")
        validate_reading(values,length - 1,limit)
     else:
-       print("Length of the battery parameter ' ",parameter_name," ' is None!!! Cannot validate reading!!!")
+       print("Length of the battery parameter '",parameter_name,"' is None!!! Cannot validate reading!!!")
   
 def validate_reading(values, last_but_one_reading,limit):
       for i in range(last_but_one_reading):
