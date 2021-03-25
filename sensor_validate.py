6ad4618493907525e78ce30b564e04ff901b7790
PREETI <preetikadyan634@gmail.com>
2021-03-25 13:08:33 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -4,12 +4,13 @@ def  check_if_given_parameter_produces_noise(value, nextValue, maxDelta):
     return False
   return True
 
-def check_if_length_is_not_none_and_validate_reading(values,limit):
+def check_if_length_is_not_none_and_validate_reading(values,limit,parameter_name):
     length = len(values)
     if length != 0:
+       print("--------------Validate Reading for the Parameter ",parameter_name,"--------------")
        validate_reading(values,length - 1,limit)
     else:
-       print("Length of the parameter is None!!! Cannot validate reading!!!")
+       print("Length of the battery parameter ' ",parameter_soc," ' is None!!! Cannot validate reading!!!")
   
 def validate_reading(values, last_but_one_reading,limit):
       for i in range(last_but_one_reading):
