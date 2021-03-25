6d253aacbd1640d635f0724b9f165527df54f072
PREETI <preetikadyan634@gmail.com>
2021-03-25 15:41:53 +0530

    Update sensor_validate.test.py

--- sensor_validate.test.py
+++ sensor_validate.test.py
@@ -10,18 +10,18 @@ class SensorValidatorTest(unittest.TestCase):
   def test_reports_error_when_soc_jumps(self):
     
     self.assertFalse(
-      sensor_validate.check_if_length_is_not_none_and_validate_reading([0.0, 0.01, 0.5, 0.51],soc_limit,'SOC')
-    )
+      sensor_validate.if_length_is_not_none_then_validate_reading([0.0, 0.01, 0.5, 0.51],soc_limit,'SOC')
+  
   
   def test_reports_error_when_current_jumps(self):
     
     self.assertFalse(
-      sensor_validate.check_if_length_is_not_none_and_validate_reading([0.03, 0.03, 0.03, 0.33],current_limit,'CURRENT')
+      sensor_validate.if_length_is_not_none_then_validate_reading([0.03, 0.03, 0.03, 0.33],current_limit,'CURRENT')
     )
     
   def test_empty_input(self):
-      sensor_validate.check_if_length_is_not_none_and_validate_reading([],current_limit,'CURRENT')
-      sensor_validate.check_if_length_is_not_none_and_validate_reading([],soc_limit,'SOC')
+      sensor_validate.if_length_is_not_none_then_validate_readingg([],current_limit,'CURRENT')
+      sensor_validate.if_length_is_not_none_then_validate_reading([],soc_limit,'SOC')
 
 if __name__ == "__main__":
   unittest.main()
