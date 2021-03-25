937f160810ba968cc620a7dc707ee3f1bd2a1fdd
PREETI <preetikadyan634@gmail.com>
2021-03-25 13:03:30 +0530

    Update sensor_validate.test.py

--- sensor_validate.test.py
+++ sensor_validate.test.py
@@ -5,22 +5,26 @@ import sensor_validate
 soc_limit = 0.05
 current_limit = 0.1
 
+# Specifying the battery parameter
+parameter_soc = 'soc'
+parameter_current = 'current'
+
 class SensorValidatorTest(unittest.TestCase):
   def test_reports_error_when_soc_jumps(self):
     
     self.assertFalse(
-      sensor_validate.check_if_length_is_not_none_and_validate_reading([0.0, 0.01, 0.5, 0.51],soc_limit)
+      sensor_validate.check_if_length_is_not_none_and_validate_reading([0.0, 0.01, 0.5, 0.51],soc_limit,parameter_soc)
     )
   
   def test_reports_error_when_current_jumps(self):
     
     self.assertFalse(
-      sensor_validate.check_if_length_is_not_none_and_validate_reading([0.03, 0.03, 0.03, 0.33],current_limit)
+      sensor_validate.check_if_length_is_not_none_and_validate_reading([0.03, 0.03, 0.03, 0.33],current_limit,parameter_current)
     )
     
   def test_empty_input(self):
-      sensor_validate.check_if_length_is_not_none_and_validate_reading([],current_limit)
-      sensor_validate.check_if_length_is_not_none_and_validate_reading([],soc_limit)
+      sensor_validate.check_if_length_is_not_none_and_validate_reading([],current_limit,parameter_current)
+      sensor_validate.check_if_length_is_not_none_and_validate_reading([],soc_limit,parameter_soc)
 
 if __name__ == "__main__":
   unittest.main()
